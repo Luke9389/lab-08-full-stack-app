cc9f60010614ca02f77f4517ba2cf16b6b98b2bc
Luke Donahue <luke9389@gmail.com>
2019-08-22 10:07:34 -0700

    baseline components setup

---
+++ data/games.js
@@ -0,0 +1,52 @@
+module.exports = [
+    {
+        name: 'Baba Is You',
+        type: 'puzzle',
+        url: 'assets/splash-img/baba-is-you.jpg',
+        year: '2019',
+        desc: 'Baba Is You is a puzzle game where the rules you have to follow are present as blocks you can interact with. By manipulating them, you can change how the game works, repurpose things you find in the levels and cause surprising interactions!',
+        isMultiplayer: false
+    },
+    {
+        name: 'The Banner Saga',
+        type: 'tactics',
+        url: 'assets/splash-img/the-banner-saga.png',
+        year: '2014',
+        desc: 'Live through an epic role-playing Viking saga where your strategic choices directly affect your personal journey. Make allies as you travel with your caravan across this stunning yet harsh landscape. Carefully choose those who will help fight a new threat that jeopardizes an entire civilization.',
+        isMultiplayer: false
+    },
+    {
+        name: 'Cultist Simulator',
+        type: 'stategy',
+        url: 'assets/splash-img/cultist-simulator.jpg',
+        year: '2018',
+        desc: 'Seize forbidden treasures. Summon alien gods. Feed on your disciples. Cultist Simulator is a game of apocalypse and yearning from Alexis Kennedy, creator of Fallen London and Sunless Sea. Play as a seeker after unholy mysteries, in a 1920s-themed setting of hidden gods and secret histories.',
+        isMultiplayer: false
+    },
+    {
+        name: 'Hyper Light Drifter',
+        type: 'action',
+        url: 'assets/splash-img/hyper-light-drifter.jpg',
+        year: '2016',
+        desc: 'Explore a beautiful, vast and ruined world riddled with dangers and lost technologies.',
+        isMultiplayer: false
+    },
+    {
+        name: 'Into the Breach',
+        type: 'strategy',
+        url: 'assets/splash-img/into-the-breach.jpg',
+        year: '2018',
+        desc: 'Control powerful mechs from the future to defeat an alien threat. Each attempt to save the world presents a new randomly generated challenge in this turn-based strategy game.',
+        isMultiplayer: false
+    },
+    {
+        name: 'Nowhere Prophet',
+        type: 'card',
+        url: 'assets/splash-img/nowhere-prophet.png',
+        year: '2019',
+        desc: 'Prepare your decks and go on a pilgrimage through the wasteland! Nowhere Prophet is a unique single-player card game. Travel across randomly generated maps and lead your followers in deep tactical combat. Discover new cards and build your deck as you explore this strange, broken world.',
+        isMultiplayer: false
+    }
+];
+
+
